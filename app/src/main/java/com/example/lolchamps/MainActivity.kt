4ba33a797552bcb64a3b420aecbee92fa02974de
LilFeest <reyesolis.f10@gmail.com>
2023-08-01 23:39:58 -0600

    Feat -> desing of Login

--- app/src/main/java/com/example/lolchamps/MainActivity.kt
+++ app/src/main/java/com/example/lolchamps/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.lolchamps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLog: Button = findViewById(R.id.btnLog)
+        val btnReg: Button = findViewById(R.id.btnRegistrar)
+
+        btnLog.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, Login())
+                .commit()
+        }
+
+        btnReg.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, Registrarse())
+                .commit()
+        }
+
     }
+
+
 }
