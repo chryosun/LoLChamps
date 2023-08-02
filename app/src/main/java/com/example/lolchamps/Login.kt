c3d7c9e8ed4fbd20e2570156b92a2741b821edc2
Roque Castillo <titohn.2002@gmail.com>
2023-08-02 15:06:59 -0600

    arreglo Login.kt

--- app/src/main/java/com/example/lolchamps/Login.kt
+++ app/src/main/java/com/example/lolchamps/Login.kt
@@ -1,5 +1,6 @@
 package com.example.lolchamps
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -18,40 +20,45 @@ class Login : Fragment(R.layout.fragment_login) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnLogueo=view.findViewById(R.id.btnLogIngresar)
         val tBoxUser = view.findViewById<EditText>(R.id.tboxLogUsuario)
         val tBoxContra = view.findViewById<EditText>(R.id.tboxLogContra)
         val db = FirebaseFirestore.getInstance()
 
         btnLogueo.setOnClickListener {
 
-            val user_catch = tBoxUser.text.toString().trim()
-            val contra_catch = tBoxContra.text.toString().trim()
+            if (tBoxUser.text.isEmpty() || tBoxContra.text.isEmpty()) {
+                mostrar_snack_bar("No se han ingresado todos los datos...")
+            } else {
+                //show_data(name_catch,apellido_catch,correo_catch)
+                val user_catch = tBoxUser.text.toString().trim()
+                val contra_catch = tBoxContra.text.toString().trim()
 
-            db.collection("usuarios").document(tBoxUser.text.toString()).get()
-                .addOnSuccessListener { document ->
+
+                db.collection("usuarios").document(tBoxUser.text.toString()).get().addOnSuccessListener { document ->
                     document?.let {
-                        val usuario = document.toObject(Usuarios::class.java)
-                    }
-                        .addOnSuccessListener {
-                            Log.d("Ingreso de datos", "Se ingresaron los datos correctamente")
-                        }
-                        .addOnFailureListener { error ->
-                            Log.e("ErrorFirebase", "El error es ${error.toString()}")
-                        }
+                        val usuario = document.toObject(Usuario::class.java)
 
-                    if (tBoxUser.text.isEmpty() || tBoxContra.text.isEmpty()) {
-                        mostrar_snack_bar("No se han ingresado todos los datos")
-                    } else {
-                        //show_data(name_catch,apellido_catch,correo_catch)
-                    } 
+                        if (usuario?.contraseña.toString() == contra_catch){
+                            val intento= Intent(this.activity,MenuCampeones::class.java)
+                            startActivity(intento)
+                        }
+                        else{
+                            mostrar_snack_bar("Contraseña incorrecta")
+                        }
+                    }
+
+                }.addOnFailureListener{
+                    mostrar_snack_bar("Error! No existe el usuario ingresado...")
                 }
+            }
         }
     }private fun mostrar_snack_bar(mensaje: String) {
         Snackbar.make(btnLogueo, mensaje, Snackbar.LENGTH_SHORT).show()
     }
 
 }
-data class Usuarios(
-    val usuario: String = "",
-    val contra: String = ""
+data class Usuario(
+    val nombre: String = "",
+    val contraseña: String = ""
 )
